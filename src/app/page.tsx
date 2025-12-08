819de494b5fecfffd56184c68d4cfcc3aff4da21
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2025-12-08 10:49:41 +0100

    add more mockup daya

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,71 +79,91 @@ const CardTilesMock = [
     image: "/images/garlic-sauce-1.webp",
     ingredients: ["Sour cream", "Garlic", "Spices"],
     category: "sauces",
-    sizes: [{ size: 100, price: 5 }],
+    sizes: [{ size: 50, price: 3 }],
   },
   {
     name: "BBQ sauce",
     image: "/images/bbq.jpg",
     ingredients: ["Tomatoes", "Paprika", "Onion", "Spices"],
     category: "sauces",
-    sizes: [{ size: 100, price: 5 }],
+    sizes: [{ size: 50, price: 3 }],
   },
   {
     name: "Spicy sauce",
     image: "/images/bbq.jpg",
     ingredients: ["Tomatoes", "Chili", "Paprika", "Spices"],
     category: "sauces",
-    sizes: [{ size: 100, price: 5 }],
+    sizes: [{ size: 50, price: 3 }],
   },
   {
     name: "Coca-Cola",
-    image: "/images/pepperoni.jpg",
+    image: "/images/cola.jpg",
     ingredients: ["Carbonated drink"],
     category: "drinks",
     sizes: [
-      { size: 330, price: 7 },
-      { size: 500, price: 9 },
-      { size: 1000, price: 12 },
+      { size: 250, price: 7 },
+      { size: 800, price: 9 },
     ],
   },
   {
     name: "Sprite",
-    image: "/images/pepperoni.jpg",
+    image: "/images/cola.jpg",
     ingredients: ["Lemon-lime carbonated drink"],
     category: "drinks",
     sizes: [
-      { size: 330, price: 7 },
-      { size: 500, price: 9 },
+      { size: 250, price: 7 },
+      { size: 800, price: 9 },
     ],
   },
   {
     name: "Orange juice",
-    image: "/images/pepperoni.jpg",
+    image: "/images/cola.jpg",
     ingredients: ["100% orange juice"],
     category: "drinks",
     sizes: [
-      { size: 330, price: 8 },
-      { size: 500, price: 11 },
+      { size: 250, price: 7 },
+      { size: 800, price: 9 },
     ],
   },
   {
     name: "Mineral water",
-    image: "/images/pepperoni.jpg",
+    image: "/images/cola.jpg",
     ingredients: ["Sparkling spring water"],
     category: "drinks",
     sizes: [
-      { size: 500, price: 5 },
-      { size: 1000, price: 7 },
+      { size: 250, price: 7 },
+      { size: 800, price: 9 },
     ],
   },
 ];
+
+const PizzasMock = CardTilesMock.filter((card) => card.category === "pizza");
+const SaucesMock = CardTilesMock.filter((card) => card.category === "sauces");
+const DrinksMock = CardTilesMock.filter((card) => card.category === "drinks");
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-6xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
         <div className="flex flex-col gap-4 w-full max-w-4xl">
-          {CardTilesMock.map((card, index) => (
+          <h3 className="text-3xl font-bold text-orange-700 text-center mb-4">
+            Pizzas
+          </h3>
+          {PizzasMock.map((card, index) => (
+            <CardTile key={`${card.name}-${index}`} {...card} />
+          ))}
+          <br />
+          <h3 className="text-3xl font-bold text-orange-700 text-center mb-4">
+            Sauces
+          </h3>
+          {SaucesMock.map((card, index) => (
+            <CardTile key={`${card.name}-${index}`} {...card} />
+          ))}
+          <br />
+          <h3 className="text-3xl font-bold text-orange-700 text-center mb-4">
+            Drinks
+          </h3>
+          {DrinksMock.map((card, index) => (
             <CardTile key={`${card.name}-${index}`} {...card} />
           ))}
         </div>
