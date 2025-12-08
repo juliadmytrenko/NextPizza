e0eca051d4a06dac8a1e4009ca3fd2f91ef1d12b
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2025-12-08 11:03:34 +0100

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="border-t-2 border-orange-200 shadow-md bg-gradient-to-r from-white to-orange-50 mt-auto">
+      <div className="container mx-auto px-4 py-6">
+        <p className="text-center text-gray-700 font-medium">
+          © 2025 Pizza app by Julia Dmytrenko
+        </p>
+      </div>
+    </footer>
+  );
+};
