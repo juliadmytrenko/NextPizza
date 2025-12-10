498f6e53eee86f29987b113f61aa0d6e0a9ed54a
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2025-12-10 21:00:28 +0100

    fix cart (koszyk) Story

--- components/Cart.stories.tsx
+++ components/Cart.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import { Cart } from "./Cart";
-import { CartProvider } from "../context/CartContext";
+import { CartProvider, useCart } from "../context/CartContext";
+import React from "react";
 
 const meta = {
   title: "Components/Cart",
@@ -23,19 +24,42 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
 
+const CartWithItemsWrapper = () => {
+  const { addToCart, setIsCartOpen } = useCart();
+
+  React.useEffect(() => {
+    // Add items to cart
+    addToCart({
+      name: "Margherita Pizza",
+      size: 30,
+      price: 20,
+      image: "/images/margherita.jpg",
+    });
+    addToCart({
+      name: "Pepperoni Pizza",
+      size: 40,
+      price: 25,
+      image: "/images/pepperoni.jpg",
+    });
+    addToCart({
+      name: "Hawaiian Pizza",
+      size: 30,
+      price: 26,
+      image: "/images/hawaii.jpg",
+    });
+    // Open cart automatically
+    setTimeout(() => {
+      setIsCartOpen(true);
+    }, 100);
+  }, []);
+
+  return <Cart />;
+};
+
 export const WithItems: Story = {
-  decorators: [
-    (Story) => {
-      const { addToCart } = require("../context/CartContext").useCart();
-      React.useEffect(() => {
-        addToCart({
-          name: "Margherita Pizza",
-          size: 30,
-          price: 20,
-          image: "/images/margherita.jpg",
-        });
-      }, []);
-      return <Story />;
-    },
-  ],
+  render: () => (
+    <CartProvider>
+      <CartWithItemsWrapper />
+    </CartProvider>
+  ),
 };
