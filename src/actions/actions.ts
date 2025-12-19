e732d80b3a40d9b3ed013d6cf4c0c1c97255589e
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2025-12-19 14:41:35 +0100

    comment out action for now

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,11 +1,13 @@
 'use server';
 
-import { prisma } from "@/lib/prisma";
+// import { prisma } from "@/lib/prisma";
 
-export async function createIngredient(name: string) {
-    await prisma.ingredient.create({
-        data: {
-            name,
-        }
-    });
-}
+// export async function createIngredient(name: string) {
+//     await prisma.ingredient.create({
+//         data: {
+//             name,
+//         }
+//     });
+// }
+
+// USE these functions in api route files instead
