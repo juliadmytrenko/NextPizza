d173b3ef9845f526b0b93f0a1f3309e30cbff9af
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2025-12-25 12:46:56 +0100

    add domain for images of pizza

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "leclerc-online.pl"],
+    domains: ["images.unsplash.com", "leclerc-online.pl","www.przyslijprzepis.pl"],
   },
 };
 
