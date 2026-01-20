b84d9b97ce535710f8df6b0261d1994e813a75e1
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2026-01-20 14:23:01 +0100

    feat: update SignIn component to handle authentication and redirect

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const SignIn = () => {
+const SignIn = async () => {
+  const session = await auth();
+  if (session) redirect('/');
+
   return <div>SignIn</div>;
 };
 
